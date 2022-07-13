3d88bb278fee00405c934aa25752579523a314d0
kotakawase <kottan00@gmail.com>
2022-07-13 16:46:55 +0900

    削除機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :tasks, only: [:index, :show, :new, :edit, :update, :create]
+  resources :tasks, only: [:index, :show, :new, :edit, :update, :destroy, :create]
   root to: 'tasks#index'
 end
